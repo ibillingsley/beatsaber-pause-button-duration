b822ae0132e09c043144450f580373422aab1a5e
Isaiah Billingsley <iza.billingsley@gmail.com>
2023-04-24 01:56:54 -0400

    Simplify plugin init

--- PauseButtonDuration/Plugin.cs
+++ PauseButtonDuration/Plugin.cs
@@ -9,6 +9,7 @@
  */
 
 using System;
+using System.Reflection;
 using BeatSaberMarkupLanguage.Settings;
 using IPA;
 using IPA.Config.Stores;
@@ -17,7 +18,7 @@ using IPALogger = IPA.Logging.Logger;
 
 namespace PauseButtonDuration
 {
-    [Plugin(RuntimeOptions.DynamicInit)]
+    [Plugin(RuntimeOptions.SingleStartInit)]
     public class Plugin
     {
         public const string HarmonyId = "com.github.btoersche.beatsaber.PauseButtonDuration";
@@ -26,86 +27,30 @@ namespace PauseButtonDuration
         internal static IPALogger Log { get; private set; }
 
         [Init]
-        /// <summary>
-        /// Called when the plugin is first loaded by IPA (either when the game starts or when the plugin is enabled if it starts disabled).
-        /// [Init] methods that use a Constructor or called before regular methods like InitWithConfig.
-        /// Only use [Init] with one Constructor.
-        /// </summary>
-        public Plugin(IPALogger logger)
+        public void InitWithConfig(IPALogger logger, IPA.Config.Config config)
         {
             Instance = this;
-            Plugin.Log = logger;
-            Plugin.Log?.Debug("Logger initialized.");
+            Log = logger;
+            Log.Debug("PauseButtonDuration initialized.");
+
+            Configuration.PluginConfig.Instance = config.Generated<Configuration.PluginConfig>();
+            Log.Debug("Config loaded.");
         }
 
-        #region BSIPA Config
-        [Init]
-        public void InitWithConfig(IPA.Config.Config config)
+        [OnStart]
+        public void OnApplicationStart()
         {
-            Configuration.PluginConfig.Instance = config.Generated<Configuration.PluginConfig>();
-            Plugin.Log?.Debug("Config loaded.");
+            Log.Debug("Applying Harmony patches.");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
 
             BSMLSettings.instance.AddSettingsMenu("<size=85%>Pause Button Duration</size>", "PauseButtonDuration.UI.Settings.bsml", SettingsController.instance);
         }
-        #endregion
 
-
-        #region Disableable
-        /// <summary>
-        /// Called when the plugin is enabled (including when the game starts if the plugin is enabled).
-        /// </summary>
-        [OnEnable]
-        public void OnEnable()
+        [OnExit]
+        public void OnApplicationQuit()
         {
-            ApplyHarmonyPatches();
+            Log.Debug("Removing Harmony patches.");
+            harmony.UnpatchSelf();
         }
-
-        /// <summary>
-        /// Called when the plugin is disabled and on Beat Saber quit. It is important to clean up any Harmony patches, GameObjects, and Monobehaviours here.
-        /// The game should be left in a state as if the plugin was never started.
-        /// Methods marked [OnDisable] must return void or Task.
-        /// </summary>
-        [OnDisable]
-        public void OnDisable()
-        {
-            RemoveHarmonyPatches();
-        }
-        #endregion
-
-        #region Harmony
-        /// <summary>
-        /// Attempts to apply all the Harmony patches in this assembly.
-        /// </summary>
-        internal static void ApplyHarmonyPatches()
-        {
-            try
-            {
-                Plugin.Log?.Debug("Applying Harmony patches.");
-                harmony.PatchAll();
-            }
-            catch (Exception ex)
-            {
-                Plugin.Log?.Error("Error applying Harmony patches: " + ex.Message);
-                Plugin.Log?.Debug(ex);
-            }
-        }
-
-        /// <summary>
-        /// Attempts to remove all the Harmony patches that used our HarmonyId.
-        /// </summary>
-        internal static void RemoveHarmonyPatches()
-        {
-            try
-            {
-                Plugin.Log?.Debug("Removing Harmony patches.");
-                harmony.UnpatchSelf(); // Removes all patches with this HarmonyId
-            }
-            catch (Exception ex)
-            {
-                Plugin.Log?.Error("Error removing Harmony patches: " + ex.Message);
-                Plugin.Log?.Debug(ex);
-            }
-        }
-        #endregion
     }
 }
